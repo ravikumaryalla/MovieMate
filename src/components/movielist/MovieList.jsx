ab2151298f9b2c05a6a92d7288d61feafb568163
ravikumaryalla2 <veera.yalla@instinctinnovations.com>
2025-02-03 23:02:08 +0530

    used custuom hoot to fetch api

--- src/components/movielist/MovieList.jsx
+++ src/components/movielist/MovieList.jsx
@@ -1,25 +1,9 @@
 /* eslint-disable react/prop-types */
 import MovieCard from "../moviecard/MovieCard";
-import movieApi from "../../api/Api";
-import { useEffect, useState } from "react";
+import useFetchMovies from "../../hooks/useFetchMovies";
 
 const MovieList = ({ categeory }) => {
-  console.log(categeory);
-  const [movies, setMovies] = useState([]);
-
-  useEffect(() => {
-    async function getMovies() {
-      try {
-        const { data } = await movieApi.get(`/movie/${categeory}`);
-        setMovies(data.results); // Store movie data in state
-      } catch (error) {
-        console.log(error);
-      }
-    }
-
-    getMovies();
-  }, [categeory]);
-
+  const movies = useFetchMovies({ categeory });
   return (
     <div className="flex  gap-[1rem] flex-wrap  py-[2rem] justify-center bg-[#262e35]">
       {movies?.map((movie, id) => (
