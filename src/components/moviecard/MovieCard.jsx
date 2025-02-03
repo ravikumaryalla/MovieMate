9a6882c0ddde93982aa1cc619bd22ee191bbda51
ravikumaryalla2 <veera.yalla@instinctinnovations.com>
2025-02-03 23:01:35 +0530

    styling changes in moviecard

--- src/components/moviecard/MovieCard.jsx
+++ src/components/moviecard/MovieCard.jsx
@@ -3,11 +3,11 @@
 const MovieCard = ({ data }) => {
   console.log(data);
   return (
-    <div className="w-[300px] rounded-[1rem] border-[1px] border-[#7e868d] p-[0.5rem] flex-col justify-center align-center">
+    <div className="w-[300px] rounded-[0.6rem] border-[1px] border-[#7e868d] p-[0.5rem] flex-col justify-center align-center">
       <img
         src={`https://image.tmdb.org/t/p/w342${data?.poster_path}?`}
         alt="movie-card"
-        className="rounded-[1rem]"
+        className="rounded-[0.6rem]"
       />
       <h1>{data?.title}</h1>
       <p className="w-full h-[5.1rem] overflow-hidden overflow-ellipsis text-[0.7rem]">
